cb08390e7b87a9a97b259ae214555b635639c6af
Valentin Brclz <ValentinBrclz@users.noreply.github.com>
2016-07-20 22:45:52 +0200

    Handles "domain" option

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+// Saves options to chrome.storage
+function save_options() {
+	var domain = document.getElementById('domain').value;
+	chrome.storage.sync.set({domain: domain}, function() {
+
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		var saveBtn = document.getElementById('save');
+
+		status.textContent = chrome.i18n.getMessage('successSavingOptions');
+		saveBtn.disabled = true;
+
+		setTimeout(function() {
+			status.textContent = ' ';
+			saveBtn.disabled = false;
+		}, 1000);
+	});
+}
+
+// Restores state using the preferences stored in chrome.storage.
+function restore_options() {
+	chrome.storage.sync.get({domain: 'default.com'}, function(item) {
+		document.getElementById('domain').value = item.domain;
+	});
+}
+
+// Listeners
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
